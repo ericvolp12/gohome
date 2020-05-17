00e47b271a9130b0bda209d2fbc6fb3390d78c05
Eric Volpert <ericvolp12@gmail.com>
2020-05-17 11:31:59 -0500

    Indent XML blob

--- internal/outlets/wemo.go
+++ internal/outlets/wemo.go
@@ -120,12 +120,12 @@ func newSetBinaryStateMessage(on bool) string {
 
 	return fmt.Sprintf(
 		`<?xml version="1.0" encoding="utf-8"?>
-        <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-            <s:Body>
-                <u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1">
-                    <BinaryState>%v</BinaryState>
-                </u:SetBinaryState>
-            </s:Body>
-        </s:Envelope>`,
+                <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                    <s:Body>
+                        <u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1">
+                            <BinaryState>%v</BinaryState>
+                        </u:SetBinaryState>
+                    </s:Body>
+                </s:Envelope>`,
 		value)
 }
